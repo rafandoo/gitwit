7c8d534ed297aec2b60b70efe6ce43c68b5e4f14
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2026-02-03 19:52:30 -0300

    :recycle: (di): Adjustment for DI cycle safety in prod

--- src/main/java/dev/rafandoo/gitwit/di/InjectorFactory.java
+++ src/main/java/dev/rafandoo/gitwit/di/InjectorFactory.java
@@ -2,14 +2,13 @@ package dev.rafandoo.gitwit.di;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import lombok.Setter;
+import dev.rafandoo.gitwit.util.EnvironmentUtil;
 
 /**
  * Provides a shared Guice {@link Injector} instance for the application.
  */
 public final class InjectorFactory {
 
-    @Setter
     private static volatile Injector injector;
 
     /**
@@ -35,4 +34,19 @@ public final class InjectorFactory {
         return injector;
     }
 
+    /**
+     * Sets the Guice {@link Injector} instance.
+     * <p>
+     * This method is intended for use in non-production environments
+     * (e.g., testing, development) to allow for custom injector setups.
+     *
+     * @param injector the {@link Injector} to set.
+     * @throws IllegalStateException if called in a production environment.
+     */
+    public static void setInjector(Injector injector) {
+        if (EnvironmentUtil.isProd()) {
+            throw new IllegalStateException("Injector can only be set in non-production environments.");
+        }
+        InjectorFactory.injector = injector;
+    }
 }
