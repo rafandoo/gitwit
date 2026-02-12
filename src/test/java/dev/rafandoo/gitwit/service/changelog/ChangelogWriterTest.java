4b23d721a4f9ded6c472f969f385636cf8f7ec02
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2026-02-12 13:52:54 -0300

    :white_check_mark:: Adjustment of good practices in tests

--- src/test/java/dev/rafandoo/gitwit/service/changelog/ChangelogWriterTest.java
+++ src/test/java/dev/rafandoo/gitwit/service/changelog/ChangelogWriterTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("ChangelogWriter Tests")
-public class ChangelogWriterTest {
+class ChangelogWriterTest {
 
     @Mock
     GitService gitService;
